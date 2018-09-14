e2f20b1d86b8a11e351f4e9493051b2f7742904b
TitoNadar <titonadar06@gmail.com>
2018-09-15 00:15:09 +0530

    create approve request  done

--- ethereum/contracts/Campaign.sol
+++ ethereum/contracts/Campaign.sol
@@ -50,14 +50,24 @@ contract Campaign {
 
     function createRequest(string description, uint value, address recipient) public restricted {
         Request memory newRequest = Request({
-           description: description,
-           value: value,
-           recipient: recipient,
-           complete: false,
-           approvalCount: 0
+            description: description,
+            value: value,
+            recipient: recipient,
+            complete: false,
+            approvalCount: 0
         });
 
         requests.push(newRequest);
+    }
+
+    function approveRequest(uint index) public {
+        Request storage request = requests[index];
+
+        require(approvers[msg.sender]);
+        require(!request.approvals[msg.sender]);
+
+        request.approvals[msg.sender] = true;
+        request.approvalCount++;
     }
 
 }
