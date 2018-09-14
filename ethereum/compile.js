73a42a580fd00b9a25758706475fef07b0808b56
TitoNadar <titonadar06@gmail.com>
2018-09-15 00:01:48 +0530

    extracting contract adat

--- ethereum/compile.js
+++ ethereum/compile.js
@@ -12,3 +12,12 @@ const source = fs.readFileSync(campaignPath, 'utf8');
 const output = solc.compile(source, 1).contracts;    
 
 fs.ensureDirSync(buildPath);
+
+
+for (let contract in output) {
+    fs.outputJsonSync(
+      path.resolve(buildPath, contract.replace(':', '') + '.json'),
+      output[contract]
+    );
+  }
+  
