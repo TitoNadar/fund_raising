52df73564e941953bde14b4c064dd915c163263e
TitoNadar <titonadar06@gmail.com>
2018-09-15 00:01:16 +0530

    rebuilding build folder

--- ethereum/compile.js
+++ ethereum/compile.js
@@ -10,3 +10,5 @@ fs.removeSync(buildPath);
 const campaignPath = path.resolve(__dirname, 'contracts', 'Campaign.sol');
 const source = fs.readFileSync(campaignPath, 'utf8');
 const output = solc.compile(source, 1).contracts;    
+
+fs.ensureDirSync(buildPath);
