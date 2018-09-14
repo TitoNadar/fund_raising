6c61a4656db8f452e5cc9b01ab70ed5c45f70b10
TitoNadar <titonadar06@gmail.com>
2018-09-15 00:00:53 +0530

    read n compile solidity file

--- ethereum/compile.js
+++ ethereum/compile.js
@@ -2,6 +2,11 @@ const path = require('path');
 const solc = require('solc');
 const fs = require('fs-extra');
 
-
+ //deletes the folder in a single command
 const buildPath = path.resolve(__dirname, 'build');
-fs.removeSync(buildPath);             //deletes the folder in a single command
+fs.removeSync(buildPath);            
+
+//read the contract file and compiling
+const campaignPath = path.resolve(__dirname, 'contracts', 'Campaign.sol');
+const source = fs.readFileSync(campaignPath, 'utf8');
+const output = solc.compile(source, 1).contracts;    
