72d1045b4a823efd5d1c633a2323cb972d8a6e52
TitoNadar <titonadar06@gmail.com>
2018-09-15 00:21:48 +0530

    contract testing started

---
+++ tests/Campaign.tests.js
@@ -0,0 +1,12 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const web3 = new Web3(ganache.provider());
+
+const compiledFactory = require('../ethereum/build/CampaignFactory.json');
+const compiledCampaign = require('../ethereum/build/Campaign.json');
+
+let accounts;
+let factory;
+let campaignAddress;
+let campaign;
