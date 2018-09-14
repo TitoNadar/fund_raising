5ac0a1a454aa1453ecc7a2cdb07dfc9a83385ffc
TitoNadar <titonadar06@gmail.com>
2018-09-15 00:14:05 +0530

    create reuest done

--- ethereum/contracts/Campaign.sol
+++ ethereum/contracts/Campaign.sol
@@ -35,7 +35,7 @@ contract Campaign {
         _;
     }
 
-    function Campaign(uint minimum, address creator) public {
+    constructor(uint minimum, address creator) public {
         manager = creator;
         minimumContribution = minimum;
     }
@@ -45,6 +45,19 @@ contract Campaign {
 
         approvers[msg.sender] = true;
         approversCount++;
+    }
+
+
+    function createRequest(string description, uint value, address recipient) public restricted {
+        Request memory newRequest = Request({
+           description: description,
+           value: value,
+           recipient: recipient,
+           complete: false,
+           approvalCount: 0
+        });
+
+        requests.push(newRequest);
     }
 
 }
