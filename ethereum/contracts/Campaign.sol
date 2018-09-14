c39c4469c50bee438b4949c50e9af0d0a4249c27
TitoNadar <titonadar06@gmail.com>
2018-09-15 00:15:48 +0530

    finalize  request  done

--- ethereum/contracts/Campaign.sol
+++ ethereum/contracts/Campaign.sol
@@ -70,4 +70,18 @@ contract Campaign {
         request.approvalCount++;
     }
 
+
+    function finalizeRequest(uint index) public restricted {
+        Request storage request = requests[index];
+
+        require(request.approvalCount > (approversCount / 2));
+        require(!request.complete);
+
+        request.recipient.transfer(request.value);
+        request.complete = true;
+    }
+
+
+    
+
 }
