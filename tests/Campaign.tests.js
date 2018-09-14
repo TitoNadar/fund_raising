fd5a9c4ddae172114d990c6005fb18832fffc7a1
TitoNadar <titonadar06@gmail.com>
2018-09-15 00:23:06 +0530

    deploying

--- tests/Campaign.tests.js
+++ tests/Campaign.tests.js
@@ -10,3 +10,12 @@ let accounts;
 let factory;
 let campaignAddress;
 let campaign;
+
+beforeEach(async () => {
+    accounts = await web3.eth.getAccounts();
+  
+    factory = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
+      .deploy({ data: compiledFactory.bytecode })
+      .send({ from: accounts[0], gas: '1000000' });
+
+}  
