5f0814bd3686b68989ec497ca529a6167bba7500
TitoNadar <titonadar06@gmail.com>
2018-09-18 20:59:15 +0530

    contract  compiled

---
+++ ethereum/deploy.js
@@ -0,0 +1,24 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const compiledFactory = require('./build/CampaignFactory.json');
+
+const provider = new HDWalletProvider(
+  'armor master barely giraffe marriage crew hope erosion jewel topple shoot merit',
+  'https://rinkeby.infura.io/v3/d5e1672c3512408da61bbab85fb26e3c'
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(
+    JSON.parse(compiledFactory.interface)
+  )
+    .deploy({ data: compiledFactory.bytecode })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+};
+deploy();
