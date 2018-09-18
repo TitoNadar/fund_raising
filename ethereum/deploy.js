171d947777e72c75e8f853bdb156096b743b398b
TitoNadar <titonadar06@gmail.com>
2018-09-18 21:58:10 +0530

    contract deployed

--- ethereum/deploy.js
+++ ethereum/deploy.js
@@ -17,7 +17,7 @@ const deploy = async () => {
     JSON.parse(compiledFactory.interface)
   )
     .deploy({ data: compiledFactory.bytecode })
-    .send({ gas: '1000000', from: accounts[0] });
+    .send({ gas: '5000000', from: accounts[0] });
 
   console.log('Contract deployed to', result.options.address);
 };
